f773c401c7241d44ea5b85001259fcbc8abd51a7
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-02-28 12:41:40 +0100

    added SectionMain to HomeScreen

--- src/views/HomeScreen/HomeScreen.js
+++ src/views/HomeScreen/HomeScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import "./HomeScreen.scss";
 
+import bg1 from "../../assets/gallery/galeria-4.jpg";
+
 const HomeScreen = () => {
     return (
         <div className="HomeScreen">
-            
+            <div className="SectionMain" style={{
+                backgroundImage: "url('" + bg1 + "')",
+            }} >
+            <div className="bgColor">
+
+            </div>
+            </div>
         </div>
     );
 }
