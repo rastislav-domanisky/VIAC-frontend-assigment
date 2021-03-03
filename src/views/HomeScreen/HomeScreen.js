ab2ec6449a7c3e342aea4fc6543450802bd25c4f
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-03-03 14:09:07 +0100

    updated HomeScreen

--- src/views/HomeScreen/HomeScreen.js
+++ src/views/HomeScreen/HomeScreen.js
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 import Header from '../../components/Header/Header';
 import bg1 from "../../assets/gallery/galeria-4.jpg";
 import Gallery from '../../components/Gallery/Gallery';
+import Footer from '../../components/Footer/Footer';
+import Logo from '../../components/Logo/Logo';
 
 import foto1 from '../../assets/livis-1.jpg';
 import foto2 from '../../assets/livis-2.jpg';
@@ -83,6 +85,103 @@ const HomeScreen = () => {
                     <Gallery assets={[galFoto1, galFoto2, galFoto3, galFoto4, galFoto5]} />
                 </div>
             </div>
+            <div className="referencesSection">
+                <div className="referencesBtnArea">
+                    <div className="refTitleArea">
+                        <p className="refsLabel">
+                            Referencie
+                        </p>
+                        <h2 className="refsTitle">
+                            Povedali o nás
+                        </h2>
+                    </div>
+                    <div className="refBtn">
+                        Viac recenzií
+                    </div>
+                </div>
+                <div className="referencesTextArea">
+                    <p className="refsText">
+                        Lorem ipsum dolor sit amet, cons
+                        ectetuer adipiscing elit, sed diam
+                        nonummy nibh euismod tincidunt
+                        ut laoreet dolore magna aliquam
+                        erat volutpat. Ut wisi enim ad
+                        minim veniam, quis nostrud
+                        exerci tation ullamcorper suscipit
+                        lobortis nisl ut aliquip ex ea commodo consequat.
+                    </p>
+                    <p className="refsAuthor">
+                        Lorem Ipsum
+                    </p>
+                </div>
+                <div className="referencesTextArea">
+                    <p className="refsText">
+                        Lorem ipsum dolor sit amet, cons
+                        ectetuer adipiscing elit, sed diam
+                        nonummy nibh euismod tincidunt
+                        ut laoreet dolore magna aliquam
+                        erat volutpat. Ut wisi enim ad
+                        minim veniam, quis nostrud
+                        exerci tation ullamcorper suscipit
+                        lobortis nisl ut aliquip ex ea commodo consequat.
+                    </p>
+                    <p className="refsAuthor">
+                        Lorem Ipsum
+                    </p>
+                </div>
+                <div className="referencesTextArea">
+                    <p className="refsText">
+                        Lorem ipsum dolor sit amet, cons
+                        ectetuer adipiscing elit, sed diam
+                        nonummy nibh euismod tincidunt
+                        ut laoreet dolore magna aliquam
+                        erat volutpat. Ut wisi enim ad
+                        minim veniam, quis nostrud
+                        exerci tation ullamcorper suscipit
+                        lobortis nisl ut aliquip ex ea commodo consequat.
+                    </p>
+                    <p className="refsAuthor">
+                        Lorem Ipsum
+                    </p>
+                </div>
+            </div>
+            <div className="contactSection">
+                <div className="cTitleArea">
+                    <div className="cTitleLeft">
+                        <p className="cLabel">Kontakt</p>
+                        <h2 className="cTitle">Napíšte nám</h2>
+                    </div>
+                    <Logo />
+                </div>
+                <div className="cFormArea">
+                    <div className="cForm">
+                        <div className="cRow">
+                            <div className="cInput" id="cInputName">
+                                <label htmlFor="cName">Meno a priezvisko *</label>
+                                <input id="cName" type="text" name="cName" />
+                            </div>
+                            <div className="cInput" id="cInputTel">
+                                <label htmlFor="cTel">Telefón *</label>
+                                <input id="cTel" type="tel" name="cTel" />
+                            </div>
+                        </div>
+                        <div className="cRow">
+                            <div className="cInput">
+                                <label htmlFor="cMail">Emailová adresa *</label>
+                                <input id="cMail" type="email" name="cMail" />
+                            </div>
+                        </div>
+                        <div className="cRow">
+                            <div className="cInput">
+                                <label htmlFor="cMsg">Správa *</label>
+                                <textarea name="Text1" rows="5"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cImg"></div>
+                </div>
+            </div>
+            <Footer />
         </div>
     );
 }
